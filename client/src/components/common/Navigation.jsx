cf2a5b8262cd32d6023bc4abf3bdf83636881e30
Adomas Auga <hello@auga.dev>
2021-08-13 20:46:44 +0300

    removed unnecessary div from navigation jsx

--- client/src/components/common/Navigation.jsx
+++ client/src/components/common/Navigation.jsx
@@ -3,20 +3,18 @@ import { Container, Navbar, Nav } from 'react-bootstrap';
 
 export default function Navigation() {
     return (
-        <div className="navigation">
-            <Navbar bg="dark" variant="dark" className="mb-3">
-                <Container>
-                    <Navbar.Brand href="/">MeetJohn</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Item>
-                            <Nav.Link href="/">Home</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="/history">History</Nav.Link>
-                        </Nav.Item>
-                    </Nav>
-                </Container>
-            </Navbar>
-        </div>
+        <Navbar bg="dark" variant="dark" className="mb-3">
+            <Container>
+                <Navbar.Brand href="/">MeetJohn</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Item>
+                        <Nav.Link href="/">Home</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/history">History</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </Container>
+        </Navbar>
     );
 }
