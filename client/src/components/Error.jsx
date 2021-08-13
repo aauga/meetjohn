f9e27bd5d73a6d32249ec27829ff246b8e86706a
Adomas Auga <hello@auga.dev>
2021-08-13 17:29:14 +0300

    put css in javascript in error jsx

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 export default function Error() {
+    const imgWidth = {
+        width: '40%',
+    }
+
     return (
         <div className="text-center">
             <h2>Oh no! Something went wrong... :(</h2>
             <h5 className="mt-2 mb-4">The developer is fixing this right now...</h5>
-            <img src="./assets/error.gif" alt="Error" style={{width: '40%'}}/>
+            <img src="./assets/error.gif" alt="Error" style={imgWidth}/>
         </div>
     );
 }
