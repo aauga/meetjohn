af96a2b812867405824429d025e138538fcfa49b
Adomas Auga <hello@auga.dev>
2021-08-13 17:31:12 +0300

    moved error jsx to the common folder

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,4 +2,4 @@ export { default as Navigation } from './Navigation';
 export { default as Home } from './Home';
 export { default as History } from './History';
 export { default as Details } from './Details'
-export { default as Error } from './Error'
+export { default as Error } from './common/ErrorMessage'
